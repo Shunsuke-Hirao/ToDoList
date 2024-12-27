be1b3493c1b5b4a669a4aec08950dc9e7eca28cf
i-am-shunsuke <200557414@student.georgianc.on.ca>
2024-12-27 04:27:10 -0500

    Added card and dialog.xml

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -10,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+//    18:45
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
