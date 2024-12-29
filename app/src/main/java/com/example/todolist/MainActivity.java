54ad5d93a384c0f057bd833e9c75684e70a29d66
i-am-shunsuke <200557414@student.georgianc.on.ca>
2024-12-29 06:15:01 -0500

    Added more methods in main

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.todolist;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
@@ -16,7 +19,7 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
-//    58:19
+//    1:06:20
 
     Button add;
     AlertDialog dialog;
@@ -43,6 +46,43 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void buildDialog(){
+        AlertDialog.Builder builder=new AlertDialog.Builder(this);
+        View view=getLayoutInflater().inflate(R.layout.dialog, null);
+
+        final EditText name=view.findViewById(R.id.nameEdit);
+
+        builder.setView(view);
+        builder.setTitle("Enter your task")
+                .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        addCard(name.getText().toString());
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+        builder.create();
+    }
+
+    private void addCard(String name){
+        final View view=getLayoutInflater().inflate(R.layout.card, null);
+
+
+        TextView nameView =view.findViewById(R.id.name);
+        Button delete=view.findViewById(R.id.delete);
+        nameView.setText(name);
+        delete.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout.removeView(view);
+            }
+        });
+        layout.addView(view);
 
     }
 }
